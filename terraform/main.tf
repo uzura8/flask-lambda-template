3eaa59bd31b323e0ea91d5fe43c2df6e9bfd93d9
Shingo Yamada <uzuranoie@gmail.com>
2022-02-25 08:51:37 +0900

    CONTENT_API-26: aws_Added cognito_user_pool_client option

--- terraform/main.tf
+++ terraform/main.tf
@@ -123,6 +123,7 @@ resource "aws_cognito_user_pool_client" "prd" {
     "ALLOW_ADMIN_USER_PASSWORD_AUTH",
     "ALLOW_CUSTOM_AUTH",
     "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_USER_SRP_AUTH",
     "ALLOW_REFRESH_TOKEN_AUTH"
   ]
 }
