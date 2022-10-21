055fece0d2c32d25b262267baabce7be2f244cd7
Shingo Yamada <uzuranoie@gmail.com>
2022-10-21 17:38:06 +0900

    Improved to get message on error ModelInvalidParamsException

--- app/models/dynamodb/base.py
+++ app/models/dynamodb/base.py
@@ -318,4 +318,8 @@ class Base():
 
 
 class ModelInvalidParamsException(Exception):
-    pass
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
