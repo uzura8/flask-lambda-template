14298ed7fc93961690d9bc3d7f7b945745e22a66
Shingo Yamada <uzuranoie@gmail.com>
2022-12-27 15:30:29 +0900

    Fixed default value

--- src/client/js/components/organisms/AdminServiceForm.vue
+++ src/client/js/components/organisms/AdminServiceForm.vue
@@ -219,7 +219,7 @@ export default{
       serviceIdInput: '',
       label: '',
       functions: [],
-      defaultConfigs: null,
+      defaultConfigs: [],
       outerSiteUrl: '',
       frontendPostDetailUrlPrefix: '',
       mediaUploadAcceptMimetypesImage: '',
