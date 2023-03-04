8b04c76d1b50e9e628cd0245efb088b19493b81a
Shingo Yamada <uzuranoie@gmail.com>
2023-03-04 14:32:45 +0900

    CONTENT_API-200: Implemented to restore table from backup

--- develop/bin/copy_dynamodb_table_items-sample.py
+++ develop/bin/copy_dynamodb_table_items-sample.py
@@ -6,10 +6,8 @@ source_table_name = 'table-name-copied-from'
 # Table name for copy to
 destination_table_name = 'table-name-copied-to'
 
-# AWS Region
-#region_name = 'ap-northeast-1'
 
-#dynamodb = boto3.resource('dynamodb', region_name=region_name)
+# Create boto3 DynamoDB client
 dynamodb = boto3.resource('dynamodb')
 
 # Get table copied from
@@ -18,10 +16,25 @@ source_table = dynamodb.Table(source_table_name)
 # Get table copied to
 destination_table = dynamodb.Table(destination_table_name)
 
-# Get all items from table copied from
-response = source_table.scan()
+# ExclusiveStartKey to set start position for scan
+exclusive_start_key = None
 
-# Put all items to table copied to
-with destination_table.batch_writer() as batch:
-    for item in response['Items']:
-        batch.put_item(Item=item)
+while True:
+    # Get items from table copied from to set start position
+    scan_kwargs = {}
+    if exclusive_start_key:
+        scan_kwargs['ExclusiveStartKey'] = exclusive_start_key
+
+    response = source_table.scan(**scan_kwargs)
+
+    # Insert items to table copied to
+    with destination_table.batch_writer() as batch:
+        for item in response['Items']:
+            batch.put_item(Item=item)
+
+    # Break loop on not exits ExclusiveStartKey
+    if 'LastEvaluatedKey' not in response:
+        break
+
+    # Set start potions for next scan
+    exclusive_start_key = response['LastEvaluatedKey']
