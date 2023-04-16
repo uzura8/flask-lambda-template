520fc11b2d3521c298cfbac398fdc6ae0d0f8753
Shingo Yamada <uzuranoie@gmail.com>
2023-04-16 21:34:26 +0900

    Added create post link at sub navbar on admin

--- src/client/js/components/organisms/EbAdminTabMenuPost.vue
+++ src/client/js/components/organisms/EbAdminTabMenuPost.vue
@@ -6,15 +6,20 @@
         :to="`/admin/posts/${serviceId}`"
       >{{ $t('common.posts') }}</router-link>
     </li>
-    <li :class="{'is-active': this.$route.path === `/admin/posts/${serviceId}/groups`}">
+    <li :class="{'is-active': this.$route.path === `/admin/posts/${serviceId}/create`}">
       <router-link
-        :to="`/admin/posts/${serviceId}/groups`"
-      >{{ $t('page.AdminPostGroupManagement') }}</router-link>
+        :to="`/admin/posts/${serviceId}/create`"
+      >{{ $t('common.createNew') }}</router-link>
     </li>
     <li :class="{'is-active': this.$route.path === `/admin/posts/${serviceId}/categories`}">
       <router-link
         :to="`/admin/categories/${serviceId}`"
       >{{ $t('page.AdminPostCategoryManagement') }}</router-link>
+    </li>
+    <li :class="{'is-active': this.$route.path === `/admin/posts/${serviceId}/groups`}">
+      <router-link
+        :to="`/admin/posts/${serviceId}/groups`"
+      >{{ $t('page.AdminPostGroupManagement') }}</router-link>
     </li>
   </ul>
 </div>
